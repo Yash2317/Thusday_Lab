7945b4237f05130c42f2c30ef6261ec1894baac1
Yash2317 <71596965+Yash2317@users.noreply.github.com>
2022-10-13 17:43:44 +0530

    Create Question_no_01.java

---
+++ 13-10-2022/Question_no_01.java
@@ -0,0 +1,33 @@
+/*Q1)Single:
+
+WAP to create a class which contain the method Palindrom .the second class will 
+use the proerty of first class and display the output.*/
+package Thusday;
+
+import java.util.Scanner;
+
+public class Palino {
+	static Scanner sc = new Scanner(System.in);
+
+	public static void palin() {
+		
+		int num1,rev,sum = 0,temp;
+		Scanner sc = new Scanner(System.in);
+	    System.out.print("Enter your number: ");
+	    num1 = sc.nextInt();
+	    temp=num1;
+	    while(num1>0)
+	    {
+	    rev= num1%10;
+	    sum = (sum*10)+rev;
+	    num1 = num1/10;
+	}
+        if(temp==sum)
+        	System.out.println("It is a Palindrome"+temp);
+        else
+        	System.out.println("It is not a Palindrome"+temp);
+	}
+
+	}
+
+
