e792da8ad14d3d585034c7525b1ed62ebad92796
Yash2317 <71596965+Yash2317@users.noreply.github.com>
2022-10-13 17:49:04 +0530

    Update Question_no_01.java

--- 13-10-2022/Question_no_01.java
+++ 13-10-2022/Question_no_01.java
@@ -10,24 +10,35 @@ public class Palino {
 	static Scanner sc = new Scanner(System.in);
 
 	public static void palin() {
-		
-		int num1,rev,sum = 0,temp;
+
+		int num1, rev, sum = 0, temp;
 		Scanner sc = new Scanner(System.in);
-	    System.out.print("Enter your number: ");
-	    num1 = sc.nextInt();
-	    temp=num1;
-	    while(num1>0)
-	    {
-	    rev= num1%10;
-	    sum = (sum*10)+rev;
-	    num1 = num1/10;
-	}
-        if(temp==sum)
-        	System.out.println("It is a Palindrome"+temp);
-        else
-        	System.out.println("It is not a Palindrome"+temp);
+		System.out.print("Enter your number: ");
+		num1 = sc.nextInt();
+		temp = num1;
+		while (num1 > 0) // get the reverse of temp store it in variable
+		{
+			rev = num1 % 10;
+			sum = (sum * 10) + rev;
+			num1 = num1 / 10;
+		}
+		if (temp == sum) // check if sum and temp is equal
+			System.out.println("It is a Palindrome" + temp);
+		else
+			System.out.println("It is not a Palindrome" + temp);
 	}
 
+}
+
+package Thusday;
+
+public class Display extends Palino {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		palin(); // calling the palindrome function in this class 
 	}
+
+}
 
 
