5585a4a97fbf989df4d255427cc53dda09892be2
Yash2317 <71596965+Yash2317@users.noreply.github.com>
2022-10-13 20:27:43 +0530

    Update Question_no_04.java

--- 13-10-2022/Question_no_04.java
+++ 13-10-2022/Question_no_04.java
@@ -12,16 +12,16 @@ public class Sum_of_numbers {
 		// TODO Auto-generated method stub
 		Scanner sc=new Scanner(System.in);
 		int size, i, sum = 0;
-		System.out.println("enter how many number u want to display");
+		System.out.println("Enter how many number you want to add: ");
 		size = sc.nextInt();
 		int arry[] = new int[size];
-		System.out.println("enter the  numbers ");
+		System.out.println("Enter the  numbers :");
 		for (i = 0; i < size; i++) {
 			arry[i] = sc.nextInt();
 			if (arry[i] > 0) {           //to accept online positive numbers
 				sum = sum + arry[i];
 			}
 		}
-		System.out.println("sum of positive numbers is = " + sum);
+		System.out.println("Sum of positive numbers is = " + sum);
 	}
 }
