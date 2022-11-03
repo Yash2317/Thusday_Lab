4212a90c6e7b620868354c783efb2830f560996c
Yash2317 <71596965+Yash2317@users.noreply.github.com>
2022-11-03 20:48:23 +0530

    Update Q no.2.java

--- 3.11.2022/Q no.2.java
+++ 3.11.2022/Q no.2.java
@@ -5,76 +5,76 @@ who share a common, fixed-size buffer used as a queue. The producer generates a 
 */
 package Thursday03;
 
-public class Consumer_Class  extends Thread {
+public class Consumer  extends Thread {
 	
-	Problem_Class Shop;	//declared variable for using the class name.........
-	      int num,value = 0,i;  //declared varaible and data type......
+	      Prob Shop;	                 //declared variable for using the class name.........
+	      int num,value = 0,i;               //declared varaible and data type......
 	     
 	      //create construtor.............
-	      public Consumer2(Problem_Class c, int number)
+	      public Consumer1(Prob c, int number)
 	      {
 	            Shop = c;
-	            this.num = number;//use this keyword for use the same class variable...........
+	            this.num = number;             //use this keyword for use the same class variable...........
 	      }
-	      public void run()//create run method......
+	      public void run()                    //create run method......
 	      {
-	            for ( i = 0; i < 10; i++)//using the for loop
+	            for ( i = 0; i < 10; i++)
 	            {
-	                  System.out.println("Consumed value " + this.num+ " got: " + value);//for print
+	                  System.out.println("Consumed value " + this.num+ " got: " + value);
 	            }
 	      }
 
 }
 ---------------
 package Revision;
-class Main_Class // main class
+class Main_Class 
 {
 	public static void main(String[] args) // main method
 	{
-		Problem_Class pc = new Problem_Class(); // creating object of Problem class
-		Producer_Class p1 = new Producer_Class(pc, 1); // creating object of Producer_class and passing the arguments to the constructor
-		Consumer_Class c1 = new Consumer_Class(pc, 2);  // creating object of Consumer_class and passing the arguments to the constructor
-	      c1.start(); // calling a run method
-	      p1.start(); // calling a run method
+		Problem_Class pc = new Problem_Class(); 
+		Producer_Class p1 = new Producer_Class(pc, 1);
+		Consumer_Class c1 = new Consumer_Class(pc, 2);  
+	      c1.start();                                  
+	      p1.start();                               
 	}
 }
 ------------------
 package Revision;
-public class Problem_Class  {  // this is problem class that carry problem according to the ques
+public class Prob {                                     // this is problem class that carry problem according to the ques
 
-   int materials;  // global variables here
+   int materials;         
     boolean available = false;
-    public synchronized int get() // synchronized method to get a true or false
+    public synchronized int get()                        // synchronized method to get a true or false
     {
-          while (available == false) // while loop for false condition
+          while (available == false)                      // while loop for false condition
           {
                 try
                 {
-                      wait(); // if false then problem will be wait
+                      wait();                             // if false then problem will be wait
                 }
-                catch (InterruptedException ie) // catching the exception
+                catch (InterruptedException ie)           
                 {
                 }
           }
-          available = false; // material not available
+          available = false;                             // material not available
           notifyAll();
-          return materials; //return a material to the method
+          return materials;                               //return a material to the method
     }
-    public synchronized void put(int value) // synchronized method for true conditions
+    public synchronized void put(int value)               // synchronized method for true conditions
     {
-          while (available == true) // while loop for true condition
+          while (available == true)                       // while loop for true condition
           {
                 try
                 {
-                      wait(); // wait the program
+                      wait(); 
                 }
-                catch (InterruptedException ie) // catching the Interrupted Exception here
+                catch (InterruptedException ie) 
                 {
                       ie.printStackTrace();
                 }
           }
-          materials = value; // material available in a value
-          available = true; // material available here
+          materials = value;                               // material available in a value
+          available = true;                                 // material available here
           notifyAll();
     }
 
